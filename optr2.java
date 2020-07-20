ea1b838298818ba107451453ac43206097115b9b
Swetha63 <68398720+Swetha63@users.noreply.github.com>
2020-07-20 13:14:14 -0400

    Add files via upload

---
+++ optr2.java
@@ -0,0 +1,11 @@
+class Optr2{
+public static void main(String args[]){
+int x=6,y=2;
+System.out.println(x==y);//false
+System.out.println(x!=y);//true
+System.out.println(x>y);//true
+System.out.println(x<y);//false
+System.out.println(x>=y);//true
+System.out.println(x<=y);//false
+}
+}
