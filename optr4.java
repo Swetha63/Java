5d5bfc5f101d126bfef2f48fac14948b93f10120
Swetha63 <68398720+Swetha63@users.noreply.github.com>
2020-07-20 14:15:29 -0400

    Add files via upload

---
+++ optr4.java
@@ -0,0 +1,15 @@
+class Optr4{
+public static void main (String args[]){
+char x='A';
+int a=5,b=6;
+long c=54365L;
+float d=5.4f;
+double e=8.66d;
+
+System.out.println(x+a);//int
+System.out.println(a+b);//int
+System.out.println(a+c);//long
+System.out.println(c+d);//float
+System.out.println(d+e);//double
+}
+}
